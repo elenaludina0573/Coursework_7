2834c29796001cc71ab11c266b005fc4607768b4
elenaludina0573 <elenaludina0@gmail.ru>
2024-07-04 21:17:36 +0300

    Communication with telegrams

---
+++ users/services.py
@@ -0,0 +1,16 @@
+import requests
+from config import settings
+
+
+def send_telegram_message(chat_id, message):
+    """
+    Отправка сообщения в телеграмм чат
+    :param chat_id: id чата
+    :param message: текст сообщения
+    :return:
+    """
+    params = {
+        'text': message,
+        'chat_id': chat_id,
+    }
+    requests.get(f'{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendMessage', params=params)
