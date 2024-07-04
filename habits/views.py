88e355d2320a0c9084234a57992a568d97f6204c
elenaludina0573 <elenaludina0@gmail.ru>
2024-07-04 21:18:22 +0300

    Performance

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from habits.models import Habits
+from habits.serializers import HabitSerializer
+
+
+class HabitsCreateAPIView(generics.CreateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habits.objects.all()
+    permission_classes = (IsAuthenticated, )
+
+
+class HabitsListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habits.objects.all()
+    permission_classes = (AllowAny, )
+
+
+class HabitsRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habits.objects.all()
+    permission_classes = (IsAuthenticated, )
+
+
+class HabitsUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habits.objects.all()
+    permission_classes = (IsAuthenticated, )
+
+
+class HabitsDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habits.objects.all()
+    permission_classes = (IsAuthenticated, )
+
