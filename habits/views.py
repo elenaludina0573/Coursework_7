5d38e45eb16b84b541502176cb8a97a3b0633006
elenaludina0573 <elenaludina0@gmail.ru>
2024-07-05 14:22:49 +0300

    Addendum

--- habits/views.py
+++ habits/views.py
@@ -4,12 +4,13 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from habits.models import Habits
 from habits.paginators import CustomPagination
 from habits.serializers import HabitSerializer
+from users.permissions import IsOwner
 
 
 class HabitsCreateAPIView(generics.CreateAPIView):
     serializer_class = HabitSerializer
     queryset = Habits.objects.all()
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, IsOwner )
 
 
 class HabitsListAPIView(generics.ListAPIView):
@@ -22,17 +23,17 @@ class HabitsListAPIView(generics.ListAPIView):
 class HabitsRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = HabitSerializer
     queryset = Habits.objects.all()
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, IsOwner)
 
 
 class HabitsUpdateAPIView(generics.UpdateAPIView):
     serializer_class = HabitSerializer
     queryset = Habits.objects.all()
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, IsOwner)
 
 
 class HabitsDestroyAPIView(generics.DestroyAPIView):
     serializer_class = HabitSerializer
     queryset = Habits.objects.all()
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, IsOwner)
 
