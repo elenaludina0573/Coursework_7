935cbf28365c9e3e282b85f59ba7482c0d4d14c6
elenaludina0573 <elenaludina0@gmail.ru>
2024-07-08 02:01:31 +0300

    Creating a path to a list of public habits.

--- habits/services.py
+++ habits/services.py
@@ -7,7 +7,7 @@ def send_telegram_message(chat_id, message):
     Отправка сообщения в телеграмм чат
     :param chat_id: id чата
     :param message: текст сообщения
-    :return:
+    return:
     """
     params = {
         'text': message,
